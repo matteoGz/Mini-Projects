daa34b4157067f6fcebbdfa9808adf1ad62f4b44
Matteo_G <matteo.gonzi99@gmail.com>
2023-05-09 01:26:00 +0200

    fix(Weather): Fixed bugs when don't retrieve any city / photo

--- my-weather-app/src/components/Weather.js
+++ my-weather-app/src/components/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { Box, Button, FormControl, InputLabel, OutlinedInput, Typography } from '@mui/material';
+import { Button, FormControl, Grid, InputLabel, OutlinedInput, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { weatherByWmoCode } from '../models/weatherByWmoCode';
 
@@ -21,13 +21,15 @@ export default function Weather(){
     //to get coordinates of input city
         axios.get('https://nominatim.openstreetmap.org/search?q='+selectedCity+'&format=json')
             .then((response) => {
-                console.log(response.data[0]);
-                setCoordinates(response.data[0]);
+                if(response.data.length > 0){
+                    setCoordinates(response.data[0]);
+                }
             //to get selected city's photo
                 axios.get("https://pixabay.com/api/?key="+photoKey+"&q="+selectedCity)
                     .then((response)=> {
-                        console.log(response.data.hits[0]);
-                        setCityPhoto(response.data.hits[0]);    
+                        if(response.data.hits.length > 0){
+                            setCityPhoto(response.data.hits[0]);   
+                        } 
                     })
                     .catch(() => console.error("Req city photo failed"))
             //to get weather info
@@ -44,42 +46,54 @@ export default function Weather(){
 
 
     return(
-        <>
-        <FormControl sx={{ marginTop: 20}}>
-            <InputLabel htmlFor="outlined-city">City</InputLabel>
-            <OutlinedInput
-                id="outlined-city"
-                label="City"
-                sx={{ borderRadius: 10 }}
-                onChange={handleChangeCity}
-            />
-            <Button
-                variant='contained'
-                sx={{ borderRadius: 10 }}
-                endIcon={<SearchIcon/>}
-                onClick={() => searchCity()}
-            >
-                Search
-            </Button>
-        </FormControl>
-        {Object.keys(currentInfo).length !== 0 ?
-            <Box>
-                <img src={cityPhoto.webformatURL} alt={cityPhoto.tags}></img>
-                <Typography variant='h6'>
-                    Temperature:
-                    {currentInfo.current_weather.temperature + currentInfo.hourly_units.temperature_2m}
-                </Typography>
-                <Typography variant='h6'>
-                    Weather code(WMO code): 
-                    {weatherByWmoCode[currentInfo.current_weather.weathercode]}
-                </Typography>
-                <Typography variant='h6'>
-                    Precipitation probability:
-                    {currentInfo.current_weather.is_day + currentInfo.hourly_units.precipitation_probability}
-                </Typography>
-            </Box>
-        : <></>
-        }
-        </>
+        <Grid container sx={{ textAlign:'center' }}>
+            <Grid item xs={12}>
+                <FormControl sx={{ marginTop: 5, marginBottom:5 }}>
+                    <InputLabel htmlFor="outlined-city">City</InputLabel>
+                    <OutlinedInput
+                        id="outlined-city"
+                        label="City"
+                        sx={{ borderRadius: 10 }}
+                        onChange={handleChangeCity}
+                    />
+                    <Button
+                        variant='contained'
+                        sx={{ borderRadius: 10 }}
+                        endIcon={<SearchIcon/>}
+                        onClick={() => searchCity()}
+                    >
+                        Search
+                    </Button>
+                </FormControl>
+            </Grid>
+            {Object.keys(currentInfo).length !== 0 ?
+                <>
+                {cityPhoto.webformatURL !== undefined ?
+                    <Grid item xs={12}>
+                        <img src={cityPhoto.webformatURL} alt={cityPhoto.tags} style={{ borderRadius: 15 }}/>
+                    </Grid>
+                :   <></>
+                }    
+                    <Grid item xs={12}>    
+                        <Typography variant='h6'>
+                            Temperature:
+                            {currentInfo.current_weather.temperature + currentInfo.hourly_units.temperature_2m}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='h6'>
+                            Weather code(WMO code): 
+                            {weatherByWmoCode[currentInfo.current_weather.weathercode]}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='h6'>
+                            Precipitation probability:
+                            {currentInfo.current_weather.is_day + currentInfo.hourly_units.precipitation_probability}
+                        </Typography>
+                    </Grid>
+                </>
+            :   <></> }
+        </Grid>
     )
 }
