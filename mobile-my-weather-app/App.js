88868e487877567ed4d4d450843f29786a8afa5f
Matteo_G <matteo.gonzi99@gmail.com>
2023-06-15 00:27:04 +0200

    feat(Init): Demo with header & footer inside App.js
    ...to separate them in different components

--- mobile-my-weather-app/App.js
+++ mobile-my-weather-app/App.js
@@ -3,18 +3,41 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.header}>
+        <Text style={styles.header.title}>Header</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>My weather app</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footer.title}>Footer</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor:'#2196F3',
+    padding: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e0e0e0',
+    title: { fontSize: 18, fontWeight: 'bold', color: '#fff' }
+  },
+  
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer:{
+    backgroundColor:'#2196F3',
+    padding: 30,
+    borderTopWidth: 1,
+    borderTopColor: '#e0e0e0',
+    title: { fontSize: 18, fontWeight: 'bold', color: '#fff' }
+  }
 });
