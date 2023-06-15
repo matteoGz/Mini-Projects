d96bff12ec67c68958190d469615615202825164
Hpc696 <honropgiochi@gmail.com>
2023-06-15 12:02:12 +0200

    refactor: Separate 'Content' from App.js
    Content = central content between header and footer

--- mobile-my-weather-app/App.js
+++ mobile-my-weather-app/App.js
@@ -1,26 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Content from './components/Content'
 
 export default function App() {
   return (
     <>
       <Header/>
-      <View style={styles.container}>
-        <Text>My weather app</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Content/>
       <Footer/>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
---
+++ mobile-my-weather-app/components/Content.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Content(){
+    return(
+        <>
+            <View style={styles.container}>
+                <Text>My weather app</Text>
+                <StatusBar style="auto"/>
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
